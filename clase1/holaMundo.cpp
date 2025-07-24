0c2546a5d8920694bb997e1bf73134b481103e60
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-07-24 18:05:41 -0500

    update

--- clase1/holaMundo.cpp
+++ clase1/holaMundo.cpp
@@ -1,18 +1,48 @@
 #include <iostream>
-
 using namespace std;
 
-int sumaNumeros(int a, int b) {
-    return a + b;
-}
+
 int main() {
+
+    /*
+        cout -> escribir
+        cin -> leer
+    */
     
-    int numero1 = 10;
-    int numero2 = 12;
+    /*
+        Inicializacion de variables
 
-    int resultado = sumaNumeros(numero1, numero2);
+        Variables numericas:
+        - enteros: int -> -infinito <-> +infinito, ejemplo: 0, -1, 8, 192112, -8293123
 
-    cout << resultado << endl;
+        - decimales: double, float -> -infinito <-> +infinito, ejemplo: 0.1, -9.5, 8.0
+
+
+        Variables booleanas:
+
+        - Vedadero: true
+        - Falso: false 
+        - Se definen con la palabra reservada bool
+
+        Operadores Aritmeticos: +, -, 
+
+    
+    */
+
+    // Crear una variable, declarar: 
+    // Sin inicializar: tipo_variable nombre_variable ;
+    bool resultado;
+
+    // Inizializando: tipo_variable nombre_variable = true o false; true = 1, false = 0
+    bool resultado2 = 0; 
+    
+
+    int numero = 10; // declaramos y asignamos un valor
+
+    int numero2; // declaramos
+    numero2 = 2; // asignamos un valor
+
+    cout << numero2 << endl;
 
     return 0;
 }
---
+++ clase1/leerYMostrar.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // cout -> Mostrar
+    // cin -> Escribir
+    
+    int numero = 0; // declaramos una variable numero y la inicializamos en 0
+
+    cout << "Ingresa un numero: "; // Le pedimos al usuario que ingrese un numero
+    cin >> numero; // leemos el numero
+
+    cout << "El numero ingresado es: " << numero << endl; // mostrar el numero
+
+    return 0;
+}
