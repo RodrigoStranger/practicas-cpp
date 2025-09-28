03e887e3daf3f095cf10ef86028acde88c470903
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-09-27 20:05:34 -0500

    update

--- ahorcado/main.cpp
+++ ahorcado/main.cpp
@@ -4,9 +4,11 @@ int main () {
     string palabra;
     int intentos;
     int maxIntentos;
+    string categoria;
     vector<char> letrasAdivinadas;
     vector<char> letrasIncorrectas;
-    inicializarJuego(palabra, intentos, maxIntentos, animales);
-    ejecutarJuego(palabra, intentos, maxIntentos, letrasAdivinadas, letrasIncorrectas, "Animales");
+    
+    inicializarJuego(palabra, intentos, maxIntentos, categoria);
+    ejecutarJuego(palabra, intentos, maxIntentos, letrasAdivinadas, letrasIncorrectas, categoria);
     return 0;
 }
